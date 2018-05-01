89ad981c1392956859e1e6737844ad963d809136
Kareliavs <Kareliavs@users.noreply.github.com>
2018-04-30 20:44:38 -0500

    subiendo

---
+++ 42. gaion6_incompl/js/main.js
@@ -0,0 +1,59 @@
+$( document ).ready(function(){
+	
+	$(".txtDate").keyup(function () {
+        if ($(this).val().length == 2) {
+          $(this).val($(this).val() + "/");
+        } else if ($(this).val().length == 5) {
+          $(this).val($(this).val() + "/");
+        }
+	  });
+	  
+	$(".colors a").click(function(){
+		if($(this).attr("class") !="default"){
+			$("#jquery-accordion-menu").removeClass(); 
+		$("#jquery-accordion-menu").addClass("jquery-accordion-menu").addClass($(this).attr("class"));
+	}else{
+		$("#jquery-accordion-menu").removeClass(); 
+		$("#jquery-accordion-menu").addClass("jquery-accordion-menu");
+	}});
+	
+	$( '#menu-toggle' ).click(function() {
+		
+	  $( '#jquery-accordion-menu  ul li span' ).toggleClass( "hide" );
+		
+		$( '#jquery-accordion-menu' ).toggleClass( "jquery-accordion-menu-toggle" );
+		
+		$( '#container-nav-down' ).toggleClass( "container-nav-down-toggle" );
+		
+		$( '#container-down' ).toggleClass( "container-down-toggle" );
+		
+		$( '.jquery-accordion-menu-header  span' ).toggleClass( "hide" );
+		
+		$( '.jquery-accordion-menu-footer  span' ).toggleClass( "hide" );
+		
+		$( '#menu-toggle' ).toggleClass( "float-none" );
+		
+		$( '.fa-angle-left' ).toggleClass( "fa-angle-right" );
+		
+		
+		$('.jquery-accordion-menu>ul>li>.submenu').css("display", "none");
+		
+		
+	});
+	
+	$( '.jquery-accordion-menu>ul>li>a' ).click(function() {
+		
+		if($( '.jquery-accordion-menu-header span' ).hasClass('hide')){
+			$( '#menu-toggle' ).click();
+		}
+		
+	});
+	
+	$( '.fa-bars' ).click(function() {
+		
+		$( '#container-nav-down' ).toggleClass( 'container-nav-down-toggle' );
+		
+	});
+
+})
+
